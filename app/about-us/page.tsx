dc2890530b51fb9e585b07f1349e5516f7069128
atef5922 <sifat15-5922@diu.edu.bd>
2026-07-26 16:54:49 +0600

    fix

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -42,7 +42,7 @@ const teamMembers = [
   {
     name: "Abdur Rahim",
     role: "Web Developer | SEO",
-    image: "",
+    image: "/images/team/rahim.webp",
   },
   {
     name: "Shishir Khondokar",
